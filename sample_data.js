657c45fe1cd2bd4a2f9c67f0f4f1f2555b737c08
Ali Vosoughi <64918363+ali-vosoughi@users.noreply.github.com>
2025-09-18 01:40:37 -0400

    Add files via upload

---
+++ sample_data.js
@@ -0,0 +1,259 @@
+// Auto-generated sample data
+window.generatedSampleData = [
+    {
+        id: "000_252_9",
+        image: "assets/000_252_9/original_image.jpg",
+        dryAudio: "assets/000_252_9/dry_audio.wav",
+        groundTruthWet: "assets/000_252_9/ground_truth_wet.wav",
+        image2reverbWet: "assets/000_252_9/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/000_252_9/text2reverb_long_wet.wav"
+    },
+    {
+        id: "001_193_1",
+        image: "assets/001_193_1/original_image.jpg",
+        dryAudio: "assets/001_193_1/dry_audio.wav",
+        groundTruthWet: "assets/001_193_1/ground_truth_wet.wav",
+        image2reverbWet: "assets/001_193_1/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/001_193_1/text2reverb_long_wet.wav"
+    },
+    {
+        id: "002_219_4",
+        image: "assets/002_219_4/original_image.jpg",
+        dryAudio: "assets/002_219_4/dry_audio.wav",
+        groundTruthWet: "assets/002_219_4/ground_truth_wet.wav",
+        image2reverbWet: "assets/002_219_4/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/002_219_4/text2reverb_long_wet.wav"
+    },
+    {
+        id: "003_223_68",
+        image: "assets/003_223_68/original_image.jpg",
+        dryAudio: "assets/003_223_68/dry_audio.wav",
+        groundTruthWet: "assets/003_223_68/ground_truth_wet.wav",
+        image2reverbWet: "assets/003_223_68/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/003_223_68/text2reverb_long_wet.wav"
+    },
+    {
+        id: "004_210_22",
+        image: "assets/004_210_22/original_image.jpg",
+        dryAudio: "assets/004_210_22/dry_audio.wav",
+        groundTruthWet: "assets/004_210_22/ground_truth_wet.wav",
+        image2reverbWet: "assets/004_210_22/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/004_210_22/text2reverb_long_wet.wav"
+    },
+    {
+        id: "005_262_10",
+        image: "assets/005_262_10/original_image.jpg",
+        dryAudio: "assets/005_262_10/dry_audio.wav",
+        groundTruthWet: "assets/005_262_10/ground_truth_wet.wav",
+        image2reverbWet: "assets/005_262_10/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/005_262_10/text2reverb_long_wet.wav"
+    },
+    {
+        id: "006_227_17",
+        image: "assets/006_227_17/original_image.jpg",
+        dryAudio: "assets/006_227_17/dry_audio.wav",
+        groundTruthWet: "assets/006_227_17/ground_truth_wet.wav",
+        image2reverbWet: "assets/006_227_17/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/006_227_17/text2reverb_long_wet.wav"
+    },
+    {
+        id: "007_201_6",
+        image: "assets/007_201_6/original_image.jpg",
+        dryAudio: "assets/007_201_6/dry_audio.wav",
+        groundTruthWet: "assets/007_201_6/ground_truth_wet.wav",
+        image2reverbWet: "assets/007_201_6/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/007_201_6/text2reverb_long_wet.wav"
+    },
+    {
+        id: "008_231_31",
+        image: "assets/008_231_31/original_image.jpg",
+        dryAudio: "assets/008_231_31/dry_audio.wav",
+        groundTruthWet: "assets/008_231_31/ground_truth_wet.wav",
+        image2reverbWet: "assets/008_231_31/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/008_231_31/text2reverb_long_wet.wav"
+    },
+    {
+        id: "009_239_44",
+        image: "assets/009_239_44/original_image.jpg",
+        dryAudio: "assets/009_239_44/dry_audio.wav",
+        groundTruthWet: "assets/009_239_44/ground_truth_wet.wav",
+        image2reverbWet: "assets/009_239_44/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/009_239_44/text2reverb_long_wet.wav"
+    },
+    {
+        id: "010_212_19",
+        image: "assets/010_212_19/original_image.jpg",
+        dryAudio: "assets/010_212_19/dry_audio.wav",
+        groundTruthWet: "assets/010_212_19/ground_truth_wet.wav",
+        image2reverbWet: "assets/010_212_19/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/010_212_19/text2reverb_long_wet.wav"
+    },
+    {
+        id: "011_224_130",
+        image: "assets/011_224_130/original_image.jpg",
+        dryAudio: "assets/011_224_130/dry_audio.wav",
+        groundTruthWet: "assets/011_224_130/ground_truth_wet.wav",
+        image2reverbWet: "assets/011_224_130/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/011_224_130/text2reverb_long_wet.wav"
+    },
+    {
+        id: "012_220_58",
+        image: "assets/012_220_58/original_image.jpg",
+        dryAudio: "assets/012_220_58/dry_audio.wav",
+        groundTruthWet: "assets/012_220_58/ground_truth_wet.wav",
+        image2reverbWet: "assets/012_220_58/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/012_220_58/text2reverb_long_wet.wav"
+    },
+    {
+        id: "013_214_4",
+        image: "assets/013_214_4/original_image.jpg",
+        dryAudio: "assets/013_214_4/dry_audio.wav",
+        groundTruthWet: "assets/013_214_4/ground_truth_wet.wav",
+        image2reverbWet: "assets/013_214_4/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/013_214_4/text2reverb_long_wet.wav"
+    },
+    {
+        id: "014_254_20",
+        image: "assets/014_254_20/original_image.jpg",
+        dryAudio: "assets/014_254_20/dry_audio.wav",
+        groundTruthWet: "assets/014_254_20/ground_truth_wet.wav",
+        image2reverbWet: "assets/014_254_20/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/014_254_20/text2reverb_long_wet.wav"
+    },
+    {
+        id: "015_241_9",
+        image: "assets/015_241_9/original_image.jpg",
+        dryAudio: "assets/015_241_9/dry_audio.wav",
+        groundTruthWet: "assets/015_241_9/ground_truth_wet.wav",
+        image2reverbWet: "assets/015_241_9/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/015_241_9/text2reverb_long_wet.wav"
+    },
+    {
+        id: "016_227_18",
+        image: "assets/016_227_18/original_image.jpg",
+        dryAudio: "assets/016_227_18/dry_audio.wav",
+        groundTruthWet: "assets/016_227_18/ground_truth_wet.wav",
+        image2reverbWet: "assets/016_227_18/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/016_227_18/text2reverb_long_wet.wav"
+    },
+    {
+        id: "017_247_1",
+        image: "assets/017_247_1/original_image.jpg",
+        dryAudio: "assets/017_247_1/dry_audio.wav",
+        groundTruthWet: "assets/017_247_1/ground_truth_wet.wav",
+        image2reverbWet: "assets/017_247_1/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/017_247_1/text2reverb_long_wet.wav"
+    },
+    {
+        id: "018_231_10",
+        image: "assets/018_231_10/original_image.jpg",
+        dryAudio: "assets/018_231_10/dry_audio.wav",
+        groundTruthWet: "assets/018_231_10/ground_truth_wet.wav",
+        image2reverbWet: "assets/018_231_10/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/018_231_10/text2reverb_long_wet.wav"
+    },
+    {
+        id: "019_223_91",
+        image: "assets/019_223_91/original_image.jpg",
+        dryAudio: "assets/019_223_91/dry_audio.wav",
+        groundTruthWet: "assets/019_223_91/ground_truth_wet.wav",
+        image2reverbWet: "assets/019_223_91/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/019_223_91/text2reverb_long_wet.wav"
+    },
+    {
+        id: "020_227_78",
+        image: "assets/020_227_78/original_image.jpg",
+        dryAudio: "assets/020_227_78/dry_audio.wav",
+        groundTruthWet: "assets/020_227_78/ground_truth_wet.wav",
+        image2reverbWet: "assets/020_227_78/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/020_227_78/text2reverb_long_wet.wav"
+    },
+    {
+        id: "021_193_2",
+        image: "assets/021_193_2/original_image.jpg",
+        dryAudio: "assets/021_193_2/dry_audio.wav",
+        groundTruthWet: "assets/021_193_2/ground_truth_wet.wav",
+        image2reverbWet: "assets/021_193_2/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/021_193_2/text2reverb_long_wet.wav"
+    },
+    {
+        id: "022_255_37",
+        image: "assets/022_255_37/original_image.jpg",
+        dryAudio: "assets/022_255_37/dry_audio.wav",
+        groundTruthWet: "assets/022_255_37/ground_truth_wet.wav",
+        image2reverbWet: "assets/022_255_37/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/022_255_37/text2reverb_long_wet.wav"
+    },
+    {
+        id: "023_243_9",
+        image: "assets/023_243_9/original_image.jpg",
+        dryAudio: "assets/023_243_9/dry_audio.wav",
+        groundTruthWet: "assets/023_243_9/ground_truth_wet.wav",
+        image2reverbWet: "assets/023_243_9/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/023_243_9/text2reverb_long_wet.wav"
+    },
+    {
+        id: "024_220_6",
+        image: "assets/024_220_6/original_image.jpg",
+        dryAudio: "assets/024_220_6/dry_audio.wav",
+        groundTruthWet: "assets/024_220_6/ground_truth_wet.wav",
+        image2reverbWet: "assets/024_220_6/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/024_220_6/text2reverb_long_wet.wav"
+    },
+    {
+        id: "025_230_19",
+        image: "assets/025_230_19/original_image.jpg",
+        dryAudio: "assets/025_230_19/dry_audio.wav",
+        groundTruthWet: "assets/025_230_19/ground_truth_wet.wav",
+        image2reverbWet: "assets/025_230_19/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/025_230_19/text2reverb_long_wet.wav"
+    },
+    {
+        id: "026_224_59",
+        image: "assets/026_224_59/original_image.jpg",
+        dryAudio: "assets/026_224_59/dry_audio.wav",
+        groundTruthWet: "assets/026_224_59/ground_truth_wet.wav",
+        image2reverbWet: "assets/026_224_59/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/026_224_59/text2reverb_long_wet.wav"
+    },
+    {
+        id: "027_223_79",
+        image: "assets/027_223_79/original_image.jpg",
+        dryAudio: "assets/027_223_79/dry_audio.wav",
+        groundTruthWet: "assets/027_223_79/ground_truth_wet.wav",
+        image2reverbWet: "assets/027_223_79/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/027_223_79/text2reverb_long_wet.wav"
+    },
+    {
+        id: "028_210_20",
+        image: "assets/028_210_20/original_image.jpg",
+        dryAudio: "assets/028_210_20/dry_audio.wav",
+        groundTruthWet: "assets/028_210_20/ground_truth_wet.wav",
+        image2reverbWet: "assets/028_210_20/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/028_210_20/text2reverb_long_wet.wav"
+    },
+    {
+        id: "029_223_85",
+        image: "assets/029_223_85/original_image.jpg",
+        dryAudio: "assets/029_223_85/dry_audio.wav",
+        groundTruthWet: "assets/029_223_85/ground_truth_wet.wav",
+        image2reverbWet: "assets/029_223_85/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/029_223_85/text2reverb_long_wet.wav"
+    },
+    {
+        id: "030_240_1",
+        image: "assets/030_240_1/original_image.jpg",
+        dryAudio: "assets/030_240_1/dry_audio.wav",
+        groundTruthWet: "assets/030_240_1/ground_truth_wet.wav",
+        image2reverbWet: "assets/030_240_1/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/030_240_1/text2reverb_long_wet.wav"
+    },
+    {
+        id: "031_232_14",
+        image: "assets/031_232_14/original_image.jpg",
+        dryAudio: "assets/031_232_14/dry_audio.wav",
+        groundTruthWet: "assets/031_232_14/ground_truth_wet.wav",
+        image2reverbWet: "assets/031_232_14/image2reverb_wet.wav",
+        promptreverbLongWet: "assets/031_232_14/text2reverb_long_wet.wav"
+    },
+];
